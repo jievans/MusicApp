907df4d38bf21c5e23930bd8a80570425949be70
App Academy Student <student@appacademy.io>
2013-08-09 09:09:34 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicApp::Application.config.secret_token = '02363966605bc116acb1c240b436fd11753e0bb47c5ca947029c99a099010f11f24a518a78d76d8aa013da199291f63b97b130c6226f503e90d8493eab778875'
