9bd1da4688573fd5b098b9ac45a849b828038918
App Academy Student <student@appacademy.io>
2013-08-09 18:03:44 -0700

    DONE

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    lines_array = lyrics.split("\n")
+    lines_array.map!{ |line| "&#9835;" << h(line) }
+    new_lyrics = lines_array.join("\n")
+    "<pre>#{new_lyrics}</pre>".html_safe
+  end
+
 end
